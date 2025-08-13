c95859d0684f3300edc345d9b94b1b627cbe89d5
Christian Lopes <christianlopessouza@gmail.com>
2025-08-13 15:13:23 -0300

    migrate(db): fix complement repository

--- app/Infrastructure/Persistence/EmailComplementRepository.php
+++ app/Infrastructure/Persistence/EmailComplementRepository.php
@@ -6,17 +6,11 @@ use Spatie\LaravelData\Data;
 
 class EmailComplementDTO extends Data
 {
-    public string $id;
     public string $email_id;
     public array $complement_data;
-    public array $template_data;
-    public \DateTimeImmutable $created_at;
-    public \DateTimeImmutable $updated_at;
 }
 
 interface EmailComplementRepository
 {
     public function save(EmailComplementDTO $complements): void;
-    public function saveEmailComplement(EmailComplementDTO $data): void;
-    public function saveEmailComplementTemplate(EmailComplementDTO $data): void;
 }
--- app/Infrastructure/Persistence/Facades/FacadesEmailComplementRepository.php
+++ app/Infrastructure/Persistence/Facades/FacadesEmailComplementRepository.php
@@ -8,28 +8,14 @@ use Illuminate\Support\Facades\DB;
 
 class FacadesEmailComplementRepository implements EmailComplementRepository
 {
-    public function saveEmailComplement(EmailComplementDTO $data): void
+    public function save(EmailComplementDTO $data): void
     {
+        $now = new \DateTime();
         DB::table('email_complements')->insert([
             'email_id'      => $data->email_id,
             'complement_data' => json_encode($data->complement_data),
-            'created_at'    => $data->created_at,
-            'updated_at'    => $data->updated_at,
+            'created_at'    => $now,
+            'updated_at'    => $now,
         ]);
-    }
-
-    public function saveEmailComplementTemplate(EmailComplementDTO $data): void
-    {
-        DB::table('email_complements_template')->insert([
-            'client_id'     => $data->email_id,       
-            'id'            => $data->id,
-            'template' => json_encode($data->template_data),
-            'created_at'    => $data->created_at,
-            'updated_at'    => $data->updated_at,
-        ]);
-    }
-    public function save(EmailComplementDTO $data): void
-    {
-        $this->saveEmailComplement($data);
     }
 }
